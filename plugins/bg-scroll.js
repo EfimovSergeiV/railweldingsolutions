d680ba09740148ff31aa168d601bfcd7e971d44e
Сергей Ефимов <efimovsergeiv@gmail.com>
2023-08-11 09:28:55 +0300

    add:

--- plugins/bg-scroll.js
+++ plugins/bg-scroll.js
@@ -1,7 +1,7 @@
 export default defineNuxtPlugin(nuxtApp => {
   if (process.client) {
     window.addEventListener('scroll', () => {
-      const scrollTop = (window.pageYOffset || document.documentElement.scrollTop)/100;
+      const scrollTop = (window.pageYOffset || document.documentElement.scrollTop)/50;
       const background = document.querySelector('#repair');
 
       if (background) {
